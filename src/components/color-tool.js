d6a4c0d08d5947c4174fade3257c2a7c4fd3de43
Eric W. Greene <eric@ericwgreene.com>
2018-03-02 10:17:06 -0800

    added code demos

--- src/components/color-tool.js
+++ src/components/color-tool.js
@@ -2,6 +2,25 @@ import * as React from 'react';
 
 export class ColorTool extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colorName: '',
+    };
+
+    // this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = (e) => {
+
+    this.setState({
+      [ e.target.name ]: e.target.type === 'number'
+        ? Number(e.target.value)
+        : e.target.value,
+    });
+  }
+
   render() {
 
     return <div>
@@ -11,6 +30,13 @@ export class ColorTool extends React.Component {
       <ul>
         {this.props.colors.map(color => <li key={color}>{color}</li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="name-input">Color Name:</label>
+          <input id="name-input" type="text" name="colorName"
+            value={this.state.colorName} onChange={this.onChange} />
+        </div>
+      </form>
     </div>;
   }
 
